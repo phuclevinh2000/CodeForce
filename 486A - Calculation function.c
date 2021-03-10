3a284e42ed60c40b28ef681ebe19e7e2956e67ec
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-03-10 17:38:03 +0200

    Create 486A - Calculation function.c

---
+++ 486A - Calculation function.c
@@ -0,0 +1,18 @@
+//486A - Calculation function
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  long long int n,ans;
+    scanf("%lld",&n);
+    if(n%2==0){
+        ans=n/2;
+    }
+    if(n%2==1){
+        ans=(n-1)/2-n;
+    }
+    printf("%lld\n",ans);
+    return 0;
+}
+
