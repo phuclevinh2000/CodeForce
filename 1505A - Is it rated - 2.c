a3f880ef45286a7dd525f539fbb66fde94e4c2fc
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-04-01 20:28:23 +0300

    Create 1505A - Is it rated - 2.c

---
+++ 1505A - Is it rated - 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+int main(void)
+{
+  char str[200];
+  while(scanf( "%s", str) != EOF)
+  {
+    puts("NO");
+    fflush(stdout);
+  }
+  return 0;
+}
