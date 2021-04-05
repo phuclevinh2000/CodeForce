4d6c1e2face291e431f124ab3ff4972a549668a8
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-04-05 20:03:03 +0300

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,14 @@
+
+ 
+ #include <bits/stdc++.h>
+
+ using namespace std;
+
+ int main()
+ {
+    int a,b,c;
+    cin >> a >> b >> c;
+    if(b < a || c <a)  cout << "NO";
+    else cout << "YES";
+    
+ }
