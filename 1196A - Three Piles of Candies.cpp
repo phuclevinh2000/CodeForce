2d82c834fe912b3b3c6fd0ef1e7d0bbe50387635
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-04-13 21:16:02 +0300

    Create 1196A - Three Piles of Candies.cpp

---
+++ 1196A - Three Piles of Candies.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		long long int a,b,c;
+		cin >> a >> b >> c;
+		cout << (a+b+c)/2 << endl;
+	}
+}
