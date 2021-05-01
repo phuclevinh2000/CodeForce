3a2544e5d7bceb67f3f18bf17386cc59ee18448a
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-05-01 23:24:50 +0300

    Create 1312A - Two Regular Polygons.cpp

---
+++ 1312A - Two Regular Polygons.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int a,b;
+		cin >> a >> b;
+		if(a%b==0) cout << "YES\n";
+		else cout << "NO\n";
+	}
+} 
+ 
