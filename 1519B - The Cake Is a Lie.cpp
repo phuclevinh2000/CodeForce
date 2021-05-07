598e86ac434a3c72cc0d358d6bb7a7f96fbd0930
phuclevinh2000 <47014132+phuclevinh2000@users.noreply.github.com>
2021-05-07 23:48:17 +0300

    Create 1519B - The Cake Is a Lie.cpp

---
+++ 1519B - The Cake Is a Lie.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main(){
+    int t;
+	cin>>t;
+    while(t--)
+	{
+    	int n,m,k;
+    	cin >> n >> m >> k;
+    	
+    	//solve
+    	if(n*m-1==k) cout << "YES\n";
+    	else cout << "NO\n";
+    }
+}
